8f467b00aae96f2f5952160ce32980e6613afc5a
waymao <my.waymao@gmail.com>
2023-10-14 04:36:35 -0400

    working ppo

--- policies/ppo.py
+++ policies/ppo.py
@@ -1,6 +1,7 @@
 # Implementation of PPO
 # Inspired by https://hrl.boyuai.com/
-
+# Implements algorithm shown at
+#     https://spinningup.openai.com/en/latest/algorithms/ppo.html
 
 
 import torch
@@ -9,21 +10,50 @@ from torch.nn import functional as F
 import numpy as np
 from copy import deepcopy
 
+def compute_adv(gamma: float, lmbda: float, delta_N: torch.Tensor) -> torch.Tensor:
+    # Computes the generalized advantage estimator according to 
+    # http://arxiv.org/abs/1506.02438
+    device = delta_N.device
+    delta_N = delta_N.detach().numpy()
+    N = delta_N.shape[0]
+    curr_adv = 0
+    adv_list = np.zeros_like(delta_N)
+    for i, delta in enumerate(reversed(delta_N)):
+        curr_adv = gamma * lmbda * curr_adv + delta
+        adv_list[N - i - 1] = curr_adv
+    return torch.from_numpy(adv_list).to(device)
+
+def compute_rew_to_go(gamma: float, rew_N: torch.Tensor) -> torch.Tensor:
+    device = rew_N.device
+    rew_N = rew_N.detach().numpy()
+    N = rew_N.shape[0]
+    rew_tg_N = np.zeros_like(rew_N)
+    curr_discount_rew = 0
+    for i, rew in enumerate(reversed(rew_N)):
+        curr_discount_rew = curr_discount_rew * gamma + rew
+        rew_tg_N[N - i - 1] = curr_discount_rew
+    return torch.from_numpy(rew_tg_N).to(device)
+
+
 class PPO(nn.Module):
     def __init__(self, 
                  actor_module: nn.Module, 
                  critic_module: nn.Module, 
                  gamma: float=0.98, 
+                 lmbda: float=0.95,
                  lr=1e-3, lr_critic=1e-3, 
+                 eps=0.2, update_per_train=10,
                  device="cpu"):
         super().__init__()
         self.actor_module = actor_module
         self.critic_module = critic_module
         self.gamma = gamma
+        self.lmbda = lmbda
         self.device = device
         self.optimizer = torch.optim.Adam(params=actor_module.parameters(), lr=lr)
         self.critic_optimizer = torch.optim.Adam(params=critic_module.parameters(), lr=lr_critic)
-        self.I = 1
+        self.eps = eps
+        self.update_per_train = update_per_train
     
     def forward(self, x):
         x = torch.tensor(x, device=self.device)
@@ -39,26 +69,41 @@ class PPO(nn.Module):
         # next_state: NxS
         # terminated: N, bool
         # truncated: N, bool
-        state = torch.tensor(batch['state'], device=self.device)
-        next_state = torch.tensor(batch['next_state'], device=self.device)
-        reward = batch['reward']
-        action = batch['action']
-        terminated = batch['terminated']
+        state_NS = torch.tensor(batch['state'], device=self.device)
+        next_state_NS = torch.tensor(batch['next_state'], device=self.device)
+        reward_N = torch.tensor(batch['reward'], device=self.device)
+        action_N = torch.tensor(batch['action'], dtype=torch.int64, device=self.device).view(-1, 1)
+        terminated_N = torch.tensor(batch['terminated'], dtype=torch.bool, device=self.device)
 
-        self.optimizer.zero_grad()
-        self.critic_optimizer.zero_grad()
-        critic_target_value = self.critic_module(state)[0]
         with torch.no_grad():
-            critic_next_value = self.critic_module(next_state)[0] if not terminated else 0
-            delta = reward + self.gamma * critic_next_value - \
-                    critic_target_value
-        critic_loss = -delta * critic_target_value
-        actor_loss = -delta * torch.log(self.actor_module(state)[action])
-        self.I *= self.gamma
+            # compute advantage
+            actor_old_N = self.actor_module.forward(state_NS).gather(1, action_N)
+            critic_old_value_N = self.critic_module(state_NS)[0]
+            critic_old_target_N = reward_N + self.gamma * self.critic_module(next_state_NS)[0] * ~terminated_N
+            delta_N = critic_old_target_N - critic_old_value_N
+            adv_N = compute_adv(self.gamma, self.lmbda, delta_N)
+            rew_tg_N = compute_rew_to_go(self.gamma, reward_N)
 
-        critic_loss.backward()
-        actor_loss.backward()
-        
-        self.optimizer.step()
-        self.critic_optimizer.step()
+        for _ in range(self.update_per_train):
+            ### Update of Actor
+            # compute new policy output
+            self.optimizer.zero_grad()
+            actor_new_N = self.actor_module.forward(state_NS).gather(1, action_N)
+            
+            # ratio
+            ratio_N = actor_new_N / actor_old_N
+            kl = torch.sum(torch.log(ratio_N))
+            # print("kl:", kl.item())
+            clip_adv_N = torch.clamp(ratio_N, 1 - self.eps, 1 + self.eps) * adv_N
+            actor_loss = -torch.min(ratio_N * adv_N, clip_adv_N).mean()
+            # print(actor_loss.item())
+            actor_loss.backward()
+            self.optimizer.step()
+
+            ### update of critic
+            self.critic_optimizer.zero_grad()
+            # critic_loss = torch.mean(F.mse_loss(self.critic_module(state_NS).squeeze(1), rew_tg_N))
+            critic_loss = torch.mean(F.mse_loss(self.critic_module(state_NS).squeeze(1), critic_old_target_N))
+            critic_loss.backward()
+            self.critic_optimizer.step()
 
